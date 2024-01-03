85576a75db15620a9e3b0c61c7d0ff7dfc02f961
396 <99076599+g396@users.noreply.github.com>
2024-01-03 13:06:22 +0900

    Fix: 拡張関数をdata classに定義

--- app/src/main/java/sns/asteroid/api/entities/Notification.kt
+++ app/src/main/java/sns/asteroid/api/entities/Notification.kt
@@ -19,4 +19,77 @@ data class Notification(
     val emoji_reaction: EmojiReaction? = null,
 ): java.io.Serializable, ContentInterface {
     var otherAccount = listOf<Account>()
+
+    companion object {
+        fun List<Notification>.margeSameReaction(): List<Notification> {
+            return this.margeSameReaction("favourite")
+                .margeSameReaction("reblog")
+                .margeEmojiReaction()
+        }
+
+        private fun List<Notification>.margeSameReaction(notificationType: String): List<Notification> {
+            val notifications = this
+
+            // 投稿 - アクションしたユーザのリスト のMapを作る
+            val groupingFavourites = let {
+                val reactions = notifications.filter { (it.type == notificationType) and (it.status != null) }
+
+                val group = reactions.groupBy { it.status!!.id }
+
+                group.keys.associateWith { id ->
+                    group[id]!!.associate { Pair(it.account, it.id) }.keys
+                }
+            }
+
+            val list = notifications.toMutableList()
+
+            groupingFavourites.forEach { id, accounts ->
+                val first = notifications.find { (it.status?.id == id) and (it.type == notificationType) }
+                first?.otherAccount = accounts.toList()
+
+                list.removeIf { (it.status?.id == id) and (it.type == notificationType) and (it.otherAccount.isEmpty()) }
+            }
+
+            return list
+        }
+
+        private fun List<Notification>.margeEmojiReaction(): List<Notification> {
+            val shortCodes = this.associate { Pair(it.id, it.emoji_reaction?.name ?: "") }.values
+                .toSet()
+                .filter { it.isNotEmpty() }
+
+            var mutableList = this
+
+            shortCodes.forEach {
+                mutableList = mutableList.margeEmojiReaction(it)
+            }
+
+            return mutableList
+        }
+
+        private fun  List<Notification>.margeEmojiReaction(shortCode: String): List<Notification> {
+            val notifications = this
+
+            // 投稿 - アクションしたユーザのリスト のMapを作る
+            val accountsMap = let {
+                val reactions = notifications.filter { (it.emoji_reaction?.name == shortCode) and (it.status != null) }
+                val group = reactions.groupBy { it.status!!.id }
+
+                group.keys.associateWith { id ->
+                    group[id]!!.associate { Pair(it.account, it.id) }.keys
+                }
+            }
+
+            val list = notifications.toMutableList()
+
+            accountsMap.forEach { id, accounts ->
+                val first = notifications.find { (it.status?.id == id) and (it.emoji_reaction?.name == shortCode) }
+                first?.otherAccount = accounts.toList()
+
+                list.removeIf { (it.status?.id == id) and (it.emoji_reaction?.name == shortCode) and (it.otherAccount.isEmpty()) }
+            }
+
+            return list
+        }
+    }
 }
