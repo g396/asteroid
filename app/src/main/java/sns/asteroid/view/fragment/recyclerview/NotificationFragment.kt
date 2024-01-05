b31e4b13182aff63645251ea7f8fd70b07996303
396 <99076599+g396@users.noreply.github.com>
2024-01-05 21:47:12 +0900

    Update: 通知画面でストリーミングを無効化できるようにした

--- app/src/main/java/sns/asteroid/view/fragment/recyclerview/NotificationFragment.kt
+++ app/src/main/java/sns/asteroid/view/fragment/recyclerview/NotificationFragment.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.core.text.HtmlCompat
 import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
@@ -60,12 +61,21 @@ class NotificationFragment:
             binding.messageBar.textView.text = HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_COMPACT).trimEnd()
             binding.messageBar.textView.movementMethod = LinkMovementMethod()
         })
-
-        startStreaming()
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
         super.onCreateMenu(menu, menuInflater)
+
+        menu.findItem(R.id.action_streaming).also {
+            it.isVisible = true
+            if (viewModel.enableStreaming) {
+                it.isChecked = true
+                it.setIcon(R.drawable.streaming_on)
+            } else {
+                it.isChecked = false
+                it.setIcon(R.drawable.streaming_off)
+            }
+        }
 
         menu.findItem(R.id.announcements).apply {
             iconTintList = let {
@@ -81,19 +91,30 @@ class NotificationFragment:
     override fun onMenuItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.announcements -> { binding.messageBar.root.isVisible = !binding.messageBar.root.isVisible }
+            R.id.action_streaming -> {
+                item.isChecked = !item.isChecked
+                if (item.isChecked) {
+                    item.setIcon(R.drawable.streaming_on)
+                    startStreaming()
+                } else {
+                    item.setIcon(R.drawable.streaming_off)
+                    stopStreaming()
+                }
+            }
             else -> return super.onMenuItemSelected(item)
         }
         return true
     }
 
     override fun onFragmentShow() {
+        super.onFragmentShow()
         lifecycleScope.launch { viewModel.reloadCredential() }
-        if(!viewModel.isLoaded or !viewModel.streamingClient.isConnecting()) {
-            lifecycleScope.launch {
-                resumeStreaming()
-                loadLatest()
-                getAnnouncements()
-            }
+
+        if(!viewModel.isLoaded) {
+            getAnnouncements()
+        }
+        if(!viewModel.streamingClient.isConnecting() and viewModel.enableStreaming) {
+            resumeStreaming()
         }
     }
 
@@ -122,7 +143,13 @@ class NotificationFragment:
     }
 
     private fun startStreaming() {
+        Toast.makeText(requireContext(), R.string.streaming_connect, Toast.LENGTH_SHORT).show()
         lifecycleScope.launch { viewModel.startStreaming() }
+    }
+
+    private fun stopStreaming() {
+        Toast.makeText(requireContext(), R.string.streaming_disconnect, Toast.LENGTH_SHORT).show()
+        lifecycleScope.launch { viewModel.stopStreaming() }
     }
 
     private fun resumeStreaming() {
